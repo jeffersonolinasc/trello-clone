e0cb8e6604ee29f6a7c5299080479e7d5c559416
Jefferson Nascimento <jefferson.oliv@hotmail.com>
2025-10-27 20:17:48 -0300

    feat: navbar flex

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
             <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
-                <div>
+                <div className="flex items-center space-x-2">
                     <Trello />
                     <span>Trello clone</span>
                 </div>
