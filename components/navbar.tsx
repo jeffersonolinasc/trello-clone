a5fa0f10c5eb251607550e48e3ed13a2555f7356
Jefferson Nascimento <jefferson.oliv@hotmail.com>
2025-10-27 21:10:01 -0300

    clerk inicio config

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,28 @@
 "use client"
 
+import { SignInButton, SignUpButton } from "@clerk/nextjs"
 import { Trello } from "lucide-react"
+import { Button } from "./ui/button"
 
 export default function Navbar() {
     return (
         <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
             <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
                 <div className="flex items-center space-x-2">
-                    <Trello />
-                    <span>Trello clone</span>
+                    <Trello className="h-6 w-6 sm:h-8 sm:w-8"/>
+                    <span className="text-xl sm:text-2xl font-bold text-gray-900">Trello clone</span>
+                </div>
+
+                <div className="flex items-center space-x-2 sm:space-x-4">
+                    <div className="">
+                        <SignInButton>
+                            <Button variant={"ghost"} size={"sm"} className="text-xs sm:text-sm">Sign in</Button>
+                        </SignInButton>
+
+                        <SignUpButton>
+                             <Button  size={"sm"} className="text-xs sm:text-sm">Sign up</Button>
+                        </SignUpButton>
+                    </div>
                 </div>
             </div>
         </header>
